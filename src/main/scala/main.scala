656098e319823484f3afc7ad8c4eac981e8ef2a5
AhmAlgiz <a.ahmatzyanov@yandex.ru>
2023-11-23 23:54:41 +0300

    final

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,16 +1,18 @@
-import scala.concurrent.duration.DurationInt
+import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
-import akka.util.Timeout
+
+import java.lang.Thread.sleep
+import scala.concurrent.Await
 
 object Node {
   sealed trait Protocol
 
-  case class Greet(replyto: ActorRef[Int]) extends Protocol
+  case class Greet(replyto: ActorRef[Protocol]) extends Protocol
   case class Greeter() extends Protocol
   case class Decide() extends Protocol
-  case class AddFriend(ref: ActorRef[Node.Protocol]) extends Protocol
-  case class Init(name: String) extends Protocol
+  case class AddFriends(refs: List[ActorRef[Node.Protocol]]) extends Protocol
+  case class Counter() extends Protocol
 
   def behavior(name: String): Behavior[Protocol] = {
     Behaviors.setup { ctx =>
@@ -21,22 +23,32 @@ object Node {
       Behaviors.receiveMessage {
 
         case Greet(replyto) =>
-          msgCount += 1
-          println("Recieved greet msg, reply to: " + replyto)
-          replyto ! msgCount
+          ctx.log.info("Recieved greet msg, reply to: {}\n", replyto)
+          replyto ! Counter()
           Behaviors.same
 
         case Greeter() =>
+          friends.foreach( f =>
+          f ! Greet(ctx.self))
           Behaviors.same
 
-        case AddFriend(ref) =>
-          friends = ref :: friends
+        case AddFriends(refs) =>
+          friends = refs ::: friends
+          ctx.log.info("friends added: {}\n", refs)
           Behaviors.same
 
         case Decide() =>
-          println("Decided", name)
+          ctx.log.info("Decided. Reset counter.")
+          msgCount = 0
           Behaviors.same
 
+        case Counter() =>
+          msgCount += 1
+          ctx.log.info("Counter incremented: {}", msgCount)
+          if (msgCount == friends.length) {
+            ctx.self ! Decide()
+          }
+          Behaviors.same
       }
     }
   }
@@ -45,20 +57,23 @@ object Node {
 object Main extends App {
   implicit val system: ActorSystem[Node.Protocol] = ActorSystem(Node.behavior("root"), "root")
 
-  println("System started")
+  println("System started\n")
 
   val Actor1: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor1"), "actor1")
   val Actor2: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor2"), "actor2")
   val Actor3: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor3"), "actor3")
 
-  Actor1 ! Node.Decide()
+  Actor1 ! Node.AddFriends(List(Actor2, Actor3))
+  Actor2 ! Node.AddFriends(List(Actor1, Actor3))
+  Actor3 ! Node.AddFriends(List(Actor1, Actor2))
 
+  Actor1 ! Node.Greeter()
+  Actor2 ! Node.Greeter()
+  Actor3 ! Node.Greeter()
 
-
-
-  implicit val timeout: Timeout = 5.seconds
+  sleep(5000)
 
   system.terminate()
-  println("ActorSystem terminated")
+  println("ActorSystem terminated\n")
 }
 
