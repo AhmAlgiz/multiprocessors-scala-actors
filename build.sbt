2ddc72bef6087857472ad59f26d9d73b7a7042ef
AhmAlgiz <97907492+AhmAlgiz@users.noreply.github.com>
2023-11-23 22:29:19 +0300

    something simple

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.13.12"
 
+resolvers += "Akka library repository".at("https://repo.akka.io/maven")
+
 libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.8.0"
 
-libraryDependencies +=  "org.slf4j" % "slf4j-nop" % "2.0.5"
+libraryDependencies +=  "ch.qos.logback" % "logback-classic" % "1.4.7"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,26 +1,62 @@
-import akka.actor.typed.SpawnProtocol.Spawn
-
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Props, SpawnProtocol}
-import akka.actor.typed.scaladsl.AskPattern._
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.util.Timeout
 
-object Main extends App {
-  implicit val system: ActorSystem[Node.Protocol] = ActorSystem(Node.behavior, "root")
+object Node {
+  sealed trait Protocol
 
-  Console.print("System started\n")
+  case class Greet(replyto: ActorRef[Int]) extends Protocol
+  case class Greeter() extends Protocol
+  case class Decide() extends Protocol
+  case class AddFriend(ref: ActorRef[Node.Protocol]) extends Protocol
+  case class Init(name: String) extends Protocol
+
+  def behavior(name: String): Behavior[Protocol] = {
+    Behaviors.setup { ctx =>
+
+      var friends: List[ActorRef[Node.Protocol]] = List.empty
+      var msgCount: Int = 0
+
+      Behaviors.receiveMessage {
+
+        case Greet(replyto) =>
+          msgCount += 1
+          println("Recieved greet msg, reply to: " + replyto)
+          replyto ! msgCount
+          Behaviors.same
+
+        case Greeter() =>
+          Behaviors.same
+
+        case AddFriend(ref) =>
+          friends = ref :: friends
+          Behaviors.same
+
+        case Decide() =>
+          println("Decided", name)
+          Behaviors.same
+
+      }
+    }
+  }
+}
+
+object Main extends App {
+  implicit val system: ActorSystem[Node.Protocol] = ActorSystem(Node.behavior("root"), "root")
+
+  println("System started")
+
+  val Actor1: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor1"), "actor1")
+  val Actor2: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor2"), "actor2")
+  val Actor3: ActorRef[Node.Protocol] = system.systemActorOf(Node.behavior("actor3"), "actor3")
+
+  Actor1 ! Node.Decide()
+
+
+
 
   implicit val timeout: Timeout = 5.seconds
-
-  val nodes: Array[ActorRef[Node.Protocol]] = Array(
-    system.systemActorOf(Node.behavior, "node1"),
-    system.systemActorOf(Node.behavior, "node2"),
-    system.systemActorOf(Node.behavior, "node3")
-  )
-
-
 
   system.terminate()
   println("ActorSystem terminated")
